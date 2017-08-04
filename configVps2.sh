c3567b65fcfd4acaf14ee27784238c1bb1056708
hiei171 <30733770+hiei171@users.noreply.github.com>
2017-08-04 18:33:41 -0300

    Update configVps2.sh

--- configVps2.sh
+++ configVps2.sh
@@ -1,11 +1,16 @@
 sudo apt-get upgrade
 sudo apt-get update
 sudo apt-get install
- 
-sudo apt-get install git -y
+
+echo --------------------------Atualiizado------------------------------
+
+#sudo apt-get install git -y
+#dependencias
 sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
  
 git clone https://github.com/pooler/cpuminer
+
+echo ------------------------CPUMINER baixado------------------------------
  
 cd cpuminer
 chmod +x autogen.sh
@@ -14,5 +19,7 @@ chmod +x autogen.sh
 ./configure CFLAGS="-O3"
 make
 sudo make install
+
+echo ------------------------CPUMINER Configurado------------------------------
  
 sudo apt-get install screen -y
